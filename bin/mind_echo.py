3fd734cba6f10cba86037976f59e146cc5e0aa27
Qingping Hou <dave2008713@gmail.com>
2013-07-13 07:08:58 -0400

    update mind_echo example

--- bin/mind_echo.py
+++ bin/mind_echo.py
@@ -12,19 +12,25 @@ if __name__ == "__main__":
     else:
         hs = headset.Headset('/dev/ttyUSB0')
 
-    while 1:
+    hs.disconnect()
+
+    while hs.get_state() != 'connected':
         print hs.get_state()
         time.sleep(1)
         if (hs.get_state() == 'standby'):
             print 'trying to connect...'
             hs.connect()
-        if (hs.get_state() == 'connected'):
-            print 'connected, wait 3s to collect data'
-            time.sleep(3)
-            print hs.get('attention')
-            print hs.get('meditation')
-            print hs.get('rawdata')
-            print 'disconnecting...'
-            hs.disconnect()
-            hs.destroy()
-            sys.exit(0)
+
+    while True:
+        print 'connected, wait 1s to collect data'
+        time.sleep(1)
+        print hs.get('attention')
+        print hs.get('meditation')
+        #print hs.get('rawdata')
+        print hs.get('alpha_waves')
+        print hs.get('blink_strength')
+
+    print 'disconnecting...'
+    hs.disconnect()
+    hs.destroy()
+    sys.exit(0)
