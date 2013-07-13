505b1931400aab0550d71d4cf5c3f653d8f6f400
Qingping Hou <dave2008713@gmail.com>
2013-07-13 19:09:04 -0400

    update test cases

--- pymindwave/tests/test_parser.py
+++ pymindwave/tests/test_parser.py
@@ -11,13 +11,39 @@ standby_test_stream = StringIO.StringIO(
     '\x2a' # [CHKSUM]
 )
 
+sync_test_stream1 = StringIO.StringIO(
+    '\x00\x12\x71\xaa' +
+    '\xaa\xaa' + # [SYNC] sync packets
+    '\x03' + # [PLENGTH] payload length
+    '\xd4\x01\x00' + # in standby mode
+    '\x2a' # [CHKSUM]
+)
+
+sync_test_stream2 = StringIO.StringIO(
+    '\x00\x12\x71' +
+    '\xaa\xaa' + # [SYNC] sync packets
+    '\x03' + # [PLENGTH] payload length
+    '\xd4\x01\x00' + # in standby mode
+    '\x2a' # [CHKSUM]
+)
+
 disconnected_test_stream = StringIO.StringIO(
     '\xaa\xaa' + # [SYNC] sync packets
     '\x04' + # [PLENGTH] payload length
     '\xd2' + # headset disconnected
     '\x02' + # data len
     '\xa1\x6c' + # headset global ID: 0xa16c
-    '\x1d' # [CHKSUM]
+    '\x1e' # [CHKSUM]
+)
+
+raw_data_test_stream = StringIO.StringIO(
+    '\x65\x02\x00\x64\x61' +
+    '\xaa\xaa' +
+    '\x04' +
+    '\x80' +
+    '\x02' +
+    '\x00\x28' +
+    '\x55'
 )
 
 official_test_stream = StringIO.StringIO(
@@ -58,8 +84,17 @@ official_test_stream = StringIO.StringIO(
     '\x34' # [CHKSUM] (1's comp inverse of 8-bit Payload sum of 0xCB)
 )
 
+
 def test_standby_mode():
     p = parser.VirtualParser(standby_test_stream)
+    p.update()
+    assert (p.dongle_state == 'standby')
+
+def test_sync():
+    p = parser.VirtualParser(sync_test_stream1)
+    p.update()
+    assert (p.dongle_state == 'standby')
+    p = parser.VirtualParser(sync_test_stream2)
     p.update()
     assert (p.dongle_state == 'standby')
 
@@ -75,3 +110,9 @@ def test_disconnected_mode():
     p = parser.VirtualParser(disconnected_test_stream)
     p.update()
     assert (p.dongle_state == 'disconnected')
+
+def test_rawdata_test():
+    p = parser.VirtualParser(raw_data_test_stream)
+    p.update()
+    print p.raw_values
+    assert (p.raw_values == [0x28])
